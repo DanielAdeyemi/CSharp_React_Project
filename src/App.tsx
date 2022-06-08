151015c4128106d68b8d3f7868bb021968167cf6
DanielAdeyemi <adeyemidany@gmail.com>
2022-06-08 16:10:59 -0700

    create movie component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IndividualMovie from './movies/IndividualMovie';
+import { movieDTO } from './movies/movies.model';
 
 function App() {
+  const testMovie:movieDTO = {
+    id: 1,
+    title: 'Hello',
+    poster: 'https://cdn.123telugu.com/content/wp-content/uploads/2017/12/hello-review.jpg'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <IndividualMovie {... testMovie}/>
+    </>
   );
 }
 
