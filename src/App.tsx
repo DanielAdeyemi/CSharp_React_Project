42754cd378dd695b2b309140c74f45863be7161c
DanielAdeyemi <adeyemidany@gmail.com>
2022-06-11 22:28:25 -0700

    add routes to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import IndividualMovie from './movies/IndividualMovie';
-import { landingPageDTO, movieDTO } from './movies/movies.model';
-import MoviesList from './movies/MoviesList';
-import Button from './utils/Button';
-import Menu from './Menu';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import IndividualMovie from "./movies/IndividualMovie";
+import { landingPageDTO, movieDTO } from "./movies/movies.model";
+import MoviesList from "./movies/MoviesList";
+import Button from "./utils/Button";
+import Menu from "./Menu";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import IndexGenres from "./genres/IndexGenres";
 
 function App() {
-  const [movies, setMovies] = useState<landingPageDTO>({});
+	const [movies, setMovies] = useState<landingPageDTO>({});
 
-  useEffect(() => {
-    return () => {
-      const timerId = setTimeout(() => {
-        setMovies({
-          inTheaters: [{
-    id: 1,
-    title: 'Hello',
-    poster: 'https://cdn.123telugu.com/content/wp-content/uploads/2017/12/hello-review.jpg'
-  },
-{
-    id: 2,
-    title: 'Hello 2',
-    poster: 'https://cdn.123telugu.com/content/wp-content/uploads/2017/12/hello-review.jpg'
-  }],
-  upcomingReleases: [
-    {id: 1,
-    title: 'Hello3',
-    poster: 'https://www.thestatesman.com/wp-content/uploads/2019/01/Hello2.png'}, {id: 1,
-    title: 'Hello4',
-    poster: 'https://www.thestatesman.com/wp-content/uploads/2019/01/Hello2.png'}
-  ]
-        })
-      }, 1500);
-    };
-  }, []);
+	useEffect(() => {
+		return () => {
+			const timerId = setTimeout(() => {
+				setMovies({
+					inTheaters: [
+						{
+							id: 1,
+							title: "Hello",
+							poster:
+								"https://cdn.123telugu.com/content/wp-content/uploads/2017/12/hello-review.jpg",
+						},
+						{
+							id: 2,
+							title: "Hello 2",
+							poster:
+								"https://cdn.123telugu.com/content/wp-content/uploads/2017/12/hello-review.jpg",
+						},
+					],
+					upcomingReleases: [
+						{
+							id: 4,
+							title: "Hello3",
+							poster:
+								"https://www.thestatesman.com/wp-content/uploads/2019/01/Hello2.png",
+						},
+						{
+							id: 3,
+							title: "Hello4",
+							poster:
+								"https://www.thestatesman.com/wp-content/uploads/2019/01/Hello2.png",
+						},
+					],
+				});
+			}, 1500);
+		};
+	}, []);
 
-  return (
-    <>
-    <Menu />
-    <div className='container'>
-      <h3>In Theaters: </h3>
-      <MoviesList movies={movies.inTheaters}/>
-      <h3>Upcoming</h3>
-      <MoviesList movies={movies.upcomingReleases} />
-    </div>
-    </>
-  );
+	return (
+		<BrowserRouter>
+			<Menu />
+			<div className="container">
+				<Switch>
+					<Route exact path="/">
+						<h3>In Theaters: </h3>
+						<MoviesList movies={movies.inTheaters} />
+						<h3>Upcoming</h3>
+						<MoviesList movies={movies.upcomingReleases} />
+					</Route>
+					<Route exact path="/genres">
+						<IndexGenres />
+					</Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,18 +1,22 @@
-export default function Menu () {
-  return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">React Movies</a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/genres">
-                Genres
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+import { NavLink } from "react-router-dom";
+
+export default function Menu() {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<NavLink to="/" className="navbar-brand">
+					React Movies
+				</NavLink>
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink to="/genres" className="nav-link">
+								Genres
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
